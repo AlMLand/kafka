c321d97c1957455915e3c81dfd42739d91da9d17
Alex Morland <morlandalex@googlemail.com>
2023-02-06 00:01:16 +0100

    very simple kafka producer

---
+++ kafkaBasics/src/main/kotlin/com/AlMLand/ProducerDemo.kt
@@ -0,0 +1,23 @@
+package com.AlMLand
+
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+import org.slf4j.LoggerFactory
+import java.util.*
+
+private val logger = LoggerFactory.getLogger(ProducerDemo::class.java)
+
+class ProducerDemo
+
+fun main() {
+    KafkaProducer<String, String>(Properties().apply {
+        setProperty("bootstrap.servers", "127.0.0.1:9092") // or "localhost:9092"
+        setProperty("key.serializer", StringSerializer::class.java.name)
+        setProperty("value.serializer", StringSerializer::class.java.name)
+    }).run {
+        send(ProducerRecord("own_topic", "my_test_record"))
+        flush()
+        close()
+    }
+}
