19b9183c53b1f72a36e00fea83eadeaa1ef95388
Alex Morland <Alex.Morland@ottogroup.com>
2023-03-23 22:46:53 +0100

    add config validators exception

--- kafkaConnect/kafkaConnectCustom/src/main/kotlin/com/AlMLand/customConnectArchetype/exceptions/Exceptions.kt
+++ kafkaConnect/kafkaConnectCustom/src/main/kotlin/com/AlMLand/customConnectArchetype/exceptions/Exceptions.kt
@@ -2,3 +2,6 @@ package com.AlMLand.customConnectArchetype.exceptions
 
 internal class NotImplementedException(message: String, throwable: Throwable? = null) :
     RuntimeException(message, throwable)
+
+internal class ConfigDefValidatorException(message: String, throwable: Throwable? = null) :
+    RuntimeException(message, throwable)
